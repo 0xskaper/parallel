6338a85caee1b573a5029c1c355112a827b6877a
skap3r <es.rajatyadav@gmail.com>
2026-04-26 06:17:47 +0000

    32x16

--- ch_02/cuda_2_3.cu
+++ ch_02/cuda_2_3.cu
@@ -105,7 +105,7 @@ int main(int argc, char **argv) {
   cudaMemcpy(d_MatA, h_A, nBytes, cudaMemcpyHostToDevice);
   cudaMemcpy(d_MatB, h_B, nBytes, cudaMemcpyHostToDevice);
 
-  int dimx = 16;
+  int dimx = 32;
   int dimy = 16;
 
   dim3 block(dimx, dimy);
