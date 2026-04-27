1eeea93077ed58335d2d0afc9833aeaec2ca8a3c
skap3r <es.rajatyadav@gmail.com>
2026-04-27 19:41:25 -0400

    EX -> 2.2 (1023)

--- chapter_II/sumArray-timer.cu
+++ chapter_II/sumArray-timer.cu
@@ -64,7 +64,7 @@ int main(int argc, char **argv) {
   int dev = 0;
   cudaSetDevice(dev);
 
-  int nElem = 1024;
+  int nElem = 1023;
   printf("Vector size: %d\n", nElem);
 
   size_t nBytes = nElem * sizeof(float);
