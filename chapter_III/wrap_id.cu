d675c0506efa72c061d558aa2c589e744b35ccad
skap3r <es.rajatyadav@gmail.com>
2026-05-05 12:13:17 -0400

    EXAMPLE -> 3.1

---
+++ chapter_III/wrap_id.cu
@@ -0,0 +1,18 @@
+#include <cuda_runtime.h>
+#include <stdio.h>
+
+__global__ void wrapKernel() {
+  unsigned int tid = blockIdx.x * blockDim.x + threadIdx.x;
+  float ia, ib;
+  ia = ib = 0.0f;
+  ib += ia + tid;
+}
+
+int main(int agc, char **argv) {
+  int dev = 0;
+  cudaDeviceProp deviceProp;
+  cudaGetDeviceProperties(&deviceProp, dev);
+  int maxWrapsPerSM = deviceProp.maxThreadsPerBlock / 32;
+  printf("\n Max Wraps -> %d", maxWrapsPerSM);
+  return 0;
+}
